2a83e2ac9eb3135ff0a04d00bd7d0a40361d8f16
gissellesanchez <>
2018-11-12 14:41:21 -0600

    Add project page

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about',(request,response)=>{
   });
 });
 
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  });
+});
+
 app.get('/bad',(request,response)=>{
   response.send({
     errorMessage:'Unable to handle request'
